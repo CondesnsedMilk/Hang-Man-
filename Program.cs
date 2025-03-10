511490fc6dd5ebf0c42172a517b9c952b39ce9d5
CondesnsedMilk <achau103@student.hbuhsd.edu>
2025-03-10 20:48:25 +0000

    Creation

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Hang_Man_;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+
+        Console.WriteLine("We");
+    }
+
+    public void Initialize()
+    {
+        new string = chooseWord;
+        var rand = new Random();
+        List<string> wordList = new List<string>{"pumpkin", "flamingo", "sophia", "kaz", "thy", "linh", "computer", "creative", "blasphemy", "stocking"};
+        
+        chooseWord = wordList[rand.Next(0, wordList.Count)];
+    }
+}
