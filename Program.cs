25478ddcd99b3f8fc04f904ea8519c6527747547
CondesnsedMilk <achau103@student.hbuhsd.edu>
2025-03-12 20:06:33 +0000

    Incomplete

--- Program.cs
+++ Program.cs
@@ -4,15 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        var wordInList = ChooseWord();
+        string input;
 
-        Console.WriteLine();
+        Console.WriteLine("Welcome to Hang Man!");
+        Console.WriteLine("You are to choose letters one at a time to guess the hidden word BEFORE you hang our man.");
+        Console.WriteLine("You have 10 chances");
+        Console.WriteLine("Would you like to play? y/n");
+        input = Console.ReadLine();
+
     }  
+
+    static void IntialList()
+    {
+        var wordInList = ChooseWord();
+        var hint = Hint(wordInList);
+
+        for(int i=0; i < hint.Count; i++)
+        {
+            Console.Write($"{hint[i]} ");
+        }
+
+    }
+
+    static void PlayGame()
+    {
+
+    }
 
     static List<char> ChooseWord()
     {
         var rand = new Random();
-        List<string> wordList = new List<string>{"pumpkin", "flamingo", "sophia", "kaz", "thy", "linh", "computer", "creative", "blasphemy", "stocking"};
+        List<string> wordList = new List<string>{"pumpkin", "flamingo", "sophia", "kaz", "thy", "linh", "computer", "creative", "blasphemy", "stocking", "stoichiometry"};
         
         List<char> wordInList = new List<char>();
         char[] chars = wordList[rand.Next(0, wordList.Count)].ToCharArray();
@@ -24,4 +46,18 @@ class Program
 
         return wordInList;
     }
+
+    static List<char> Hint(List<char> wordInList)
+    {
+        List<char> hint = new List<char>();
+
+        for(int i=0; i < wordInList.Count; i++)
+        {
+            hint.Add('_');
+        }
+
+        return hint;
+    }
+    
+        
 }
